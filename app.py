af5d12af99cc19e88aa2cc48fd220cf91ba9b656
Dang Thanh <thanhqtd2000@gmail.com>
2026-04-02 13:25:21 +0700

    first upload flask app

--- app.py
+++ app.py
@@ -546,7 +546,13 @@ def view_result(result_id):
 # CHẠY APP
 # -------------------------
 if __name__ == '__main__':
+    import os
     with app.app_context():
         db.create_all()
         create_admin()
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    #app.run(host="0.0.0.0",port=5000,debug=True)
+    #git add .
+    #git commit -m "sua app run"
+    #git push
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
