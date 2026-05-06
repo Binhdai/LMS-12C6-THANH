dcc9dee1b001c7a1eb60669d03502be6ca7fa72a
Dang Thanh <thanhqtd2000@gmail.com>
2026-05-07 04:32:06 +0700

    update link word pdf

--- app.py
+++ app.py
@@ -716,35 +716,35 @@ def auto_fix_links():
     # 👉 map ID bài → link đúng
     mapping = {
         1: {
-            "doc": "https://docs.google.com/document/d/16JzX6aLeGkTRvl5-2SexUOY6XNRf8d3o/preview",
+            "doc": "https://docs.google.com/document/d/16JzX6aLeGkTRvl5-2SexUOY6XNRf8d3o/export?format=docx",
             "pdf": "https://drive.google.com/file/d/1WW7CuLYlqB0G1ACCByzVbUX2GA_idIL9/preview"
         },
         2: {
-            "doc": "https://docs.google.com/document/d/1m25EdvcqMBURHnH-MdTLgFkzXc_NLR1r/preview",
+            "doc": "https://docs.google.com/document/d/1m25EdvcqMBURHnH-MdTLgFkzXc_NLR1r/export?format=docx",
             "pdf": "https://drive.google.com/file/d/164_icTuacIwQMllzCvSBmtU1adf9aUIJ/preview"
         },
         3: {
-            "doc": "https://docs.google.com/document/d/1_dBW_a5do5UO745ZfUiGm4IyOofKlSpW/preview",
+            "doc": "https://docs.google.com/document/d/1_dBW_a5do5UO745ZfUiGm4IyOofKlSpW/export?format=docx",
             "pdf": "https://drive.google.com/file/d/1gXI_CUXbToJ03T4UYjjw9Q9C7gPPmwCh/preview"
         },
         4: {
-            "doc": "https://docs.google.com/document/d/12oflzpK3RSpxiAn0ufhTWEW_uRzrNuF2/preview",
+            "doc": "https://docs.google.com/document/d/12oflzpK3RSpxiAn0ufhTWEW_uRzrNuF2/export?format=docx",
             "pdf": "https://drive.google.com/file/d/15fQ4XOJqW6y7FrYp2MkTu3XxUaeEov92/preview"
         },
         5: {
-            "doc": "https://docs.google.com/document/d/1vJrUnoCMuI-n05h53sp7UNeeZ59Vnskc/preview",
+            "doc": "https://docs.google.com/document/d/1vJrUnoCMuI-n05h53sp7UNeeZ59Vnskc/export?format=docx",
             "pdf": "https://drive.google.com/file/d/1ovMdwJUDN3vcJsBaASaqnW2Jq_VAFsmX/preview"
         },
         6: {
-            "doc": "https://docs.google.com/document/d/12RXwEYcMAfVTWdCvtQF0aTtdIr5BYQvp/preview",
+            "doc": "https://docs.google.com/document/d/12RXwEYcMAfVTWdCvtQF0aTtdIr5BYQvp/export?format=docx",
             "pdf": "https://drive.google.com/file/d/1Ad39ru42_d-aCDhunZWrOUn7FIsNB2Ty/preview"
         },
         7: {
-            "doc": "https://docs.google.com/document/d/1bkYHNhvMkYNmzs06_pzGYmR8-xY_XrN/preview",
+            "doc": "https://docs.google.com/document/d/1bkYHNhvMkYNmzs06_pzGYmR8-xY_XrN_/export?format=docx",
             "pdf": "https://drive.google.com/file/d/1AekKiVgl2cDVhWtHjqRaYgQmwfLS8Dz5/preview"
         },
         8: {
-            "doc": "https://docs.google.com/document/d/1t-ymCLQkWgmskNH20ft29qF98gVNvI5j/preview",
+            "doc": "https://docs.google.com/document/d/1t-ymCLQkWgmskNH20ft29qF98gVNvI5j/export?format=docx",
             "pdf": "https://drive.google.com/file/d/1ZZZe9g692lwuTdhWTOgSm7f6r4gd9zjY/preview"
         }
 
